5c3179ecf681943cb8925b6fe2325dee00538fa5
lucasDMadrigal <lucasmadrigalb@gmail.com>
2021-09-19 20:02:50 -0300

    first commit

---
+++ src/components/TodoCounter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TodoCounter() {
+    return (
+        <div>
+            <h1>
+                Contador
+            </h1>
+        </div>
+    )
+}
+
+export { TodoCounter }
